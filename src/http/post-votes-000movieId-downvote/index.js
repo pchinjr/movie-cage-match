18c07c88f72d24d057368eca1770811dba1196fb
Paul Chin Jr <pchinjr@gmail.com>
2020-12-04 13:39:21 -0500

    decr comments

--- src/http/post-votes-000movieId-downvote/index.js
+++ src/http/post-votes-000movieId-downvote/index.js
@@ -6,8 +6,8 @@ exports.handler = arc.http.async(route)
 async function route(req) {
   let movieId = req.body.movieId
   let table = "movies"
-  let incr = await data.decr({ table, key: movieId, prop: "votes" })
-  console.log("votes incremented to", incr)
+  let decr = await data.decr({ table, key: movieId, prop: "votes" })
+  console.log("votes decremented to", decr)
   return {
     statusCode: 301,
     location: '/',
